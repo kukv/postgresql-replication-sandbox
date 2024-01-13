7534c7adcc232347cd0e32d9f56cecd363cfb6aa
kukv <rio.by.kukv@gmail.com>
2024-01-14 00:35:23 +0900

    設定を調整

--- db_replica/scripts/entrypoint.sh
+++ db_replica/scripts/entrypoint.sh
@@ -4,10 +4,11 @@ set -eux
 
 pg_basebackup \
   -h db_primary \
+  -p 5432 \
   -D ${PGDATA} \
   -S test_replication_slot \
   -X stream \
   -U replicator \
   -R || true
 
-/usr/local/bin/docker-entrypoint.sh postgres
+/usr/local/bin/docker-entrypoint.sh -c 'config_file=/etc/postgresql/postgresql.conf' -c 'hba_file=/etc/postgresql/pg_hba.conf'
