3c2234676ff011c71eadb3889d6d47791d4a2d35
kukv <rio.by.kukv@gmail.com>
2024-02-03 06:23:56 +0900

    設定を調整

--- db_replica/scripts/entrypoint.sh
+++ db_replica/scripts/entrypoint.sh
@@ -2,13 +2,20 @@
 
 set -eux
 
+export PGPASSWORD=${POSTGRES_PASSWORD}
+while ! psql -h db_primary -U ${POSTGRES_USER} -d ${POSTGRES_DB} -p 5432 -c "select 'it is running';" 2>&1; do \
+    sleep 1s; \
+done
+
 pg_basebackup \
   -h db_primary \
   -p 5432 \
   -D ${PGDATA} \
   -S test_replication_slot \
+  --progress \
   -X stream \
   -U replicator \
-  -R || true
+  -Fp \
+  -R || :
 
 /usr/local/bin/docker-entrypoint.sh -c 'config_file=/etc/postgresql/postgresql.conf' -c 'hba_file=/etc/postgresql/pg_hba.conf'
